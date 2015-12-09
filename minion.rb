da00196d157259df1aab2a527bd6621abeb4f621
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-12-09 11:03:40 -0700

    Minion checks for battle, and update healthbar

--- minion.rb
+++ minion.rb
@@ -1,17 +1,20 @@
 require "gosu"
+require_relative "healthbar"
 
 class Minion
 
-	def initialize(w, h, side, lane, image)
-		@w = w
-		@h = h
+	def initialize(w, h, side, lane, image, type)
+		@width = w
+		@height = h
 		@side = side
 		@lane = lane
 		@image = image
+		@type = type
 		@change_x = 0
 		@change_y = 0
 		@speed_x = -0.81
 		@speed_y = 0.58
+		@healthbar = Healthbar.new(20)
 		if side == "top"
 			@x = 1860.0
 			@y = 20.0
@@ -26,6 +29,7 @@ class Minion
 		@change_y = change_y
 		move
 		battle(player, 1)
+		@healthbar.update(@x + change_x, @y + change_y)
 	end
 
 	def move
@@ -39,6 +43,7 @@ class Minion
 		if @x - 5 < player.getX + player.getWidth && @x + 45 > player.getX && @y - 5 < player.getY + player.getHeight && @y + 45 > player.getY
 			@speed_x = 0
 			@speed_y = 0
+			@healthbar.changehealth(-0.25)
 		else
 			@speed_x = -0.81
 			@speed_y = 0.58
@@ -47,6 +52,23 @@ class Minion
 
 	def draw
 		@image.draw(@x + @change_x, @y + @change_y, 1)
+		@healthbar.draw
+	end
+
+	def getX
+		@x
+	end
+
+	def getY
+		@y
+	end
+
+	def getWidth
+		@width
+	end
+
+	def getHeight
+		@height
 	end
 
 end
