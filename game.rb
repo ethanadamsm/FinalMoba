a9087138bbbf459e283062a157690c06b9e04b30
Ethan Adams <ethanadamsm@s.dcsdk12.org>
2015-12-09 10:35:03 -0700

    Add reference to minion

--- game.rb
+++ game.rb
@@ -1,5 +1,6 @@
 require "gosu"
 require_relative "character"
+require_relative "minion"
 class Game < Gosu::Window
 
 	def initialize 
@@ -8,27 +9,30 @@ class Game < Gosu::Window
 		@change_x = 0
 		@change_y = -960
 		@guy = Character.new(20, 1320, 100, 50, Gosu::Image.new("guy.png"), 50, 100, 10, 3)
+		@minion = Minion.new(40, 40, "top", 1, Gosu::Image.new("minion.png"))
 	end
 
 	def update
 		if self.mouse_x > 600 && self.mouse_x < 640 && @change_x > -1280
-			@change_x -= 3
+			@change_x -= 7
 		end
 		if self.mouse_x < 40 && self.mouse_x > 0 && @change_x < 0 
-			@change_x += 3
+			@change_x += 7
 		end
 		if self.mouse_y < 40 && self.mouse_y > 0 && @change_y < 0
-			@change_y += 3
+			@change_y += 7
 		end
 		if self.mouse_y > 440 && self.mouse_y < 480 && @change_y > -960 
-			@change_y -= 3
+			@change_y -= 7
 		end
 		@guy.update(@change_x, @change_y)
+		@minion.update(@change_x, @change_y, @guy)
 	end
 
 	def draw
 		@background.draw(@change_x, @change_y, 0)
 		@guy.draw
+		@minion.draw
 	end
 
 	def needs_cursor?
